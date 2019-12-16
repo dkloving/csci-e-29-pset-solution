746901ca9d8c91ed120e8c22bdde8764bdfa1887
david <davidkloving@gmail.com>
2019-12-16 14:32:47 +0200

    refactored random forest task to be subclassable per-dataset

--- pset/tasks/random_forest.py
+++ pset/tasks/random_forest.py
@@ -4,24 +4,31 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from joblib import dump
 
+from .fold_data import SambanisSplitFolds
 
-class RandomForestEvalTask(Task):
+
+class RandomForestCVTask(Task):
     """Fits and predicts with a RandomForest model, saving the predicted probabilities on test data
 
     Input files must be npz format, containing `X` and `y` arrays.
+
+    BASE class that should be used to create a specific class for a specific dataset
     """
 
     output_path = Parameter()
-    train_file = Parameter()
-    test_file = Parameter()
+    fold_id = IntParameter()
     n_estimators = IntParameter()
+
+    def requires(self):
+        raise NotImplementedError
 
     def output(self):
         return LocalTarget(self.output_path)
 
     def run(self):
-        train_data = np.load(self.train_file)
-        test_data = np.load(self.test_file)
+        root_dir = os.path.join(self.input().path, "folds", str(self.fold_id))
+        train_data = np.load(os.path.join(root_dir, "train.npz"))
+        test_data = np.load(os.path.join(root_dir, "train.npz"))
         clf = RandomForestClassifier(n_estimators=self.n_estimators)
         clf.fit(train_data['X'], train_data['y'])
         y_pred = clf.predict_proba(test_data['X'])[:, 1]
@@ -35,3 +42,9 @@ class RandomForestEvalTask(Task):
         preds_exists = os.path.exists(os.path.join(self.output().path, "predictions.npy"))
         model_exists = os.path.exists(os.path.join(self.output().path, "model.joblib"))
         return preds_exists and model_exists
+
+
+class SambanisRandomForestCV(RandomForestCVTask):
+
+    def requires(self):
+        return SambanisSplitFolds()
