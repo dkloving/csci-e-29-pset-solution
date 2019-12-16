f795eec5071529ea695a675ae9756ea983181272
david <davidkloving@gmail.com>
2019-12-16 14:59:30 +0200

    addd cli

--- pset/cli.py
+++ pset/cli.py
@@ -1,6 +1,10 @@
 from luigi import build
 from .tasks.fold_data import SambanisSplitFolds
+from .tasks.random_forest import SambanisRandomForestCV
+from .tasks.cross_validation import CrossValidateBase
 
 
 def main():
-    build([SambanisSplitFolds()], local_scheduler=True)
+    build([CrossValidateBase(output_path='temp')]
+          , local_scheduler=True)
+    # build([SambanisSplitFolds()], local_scheduler=True)
